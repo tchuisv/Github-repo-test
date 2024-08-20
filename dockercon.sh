f3a9b577d57798bf278f38c10765cc9c2450725c
tchuiv2 <foffevoltaire@gmail.com>
2024-08-19 21:08:03 -0500

    image

---
+++ dockercon.sh
@@ -0,0 +1,14 @@
+# command to run and pull image from docker hub
+
+#!bin/bash
+sudo su
+mkdir dockdir
+cd dockdir
+yum install docker -y
+systemctl start docker.service
+docker pull voltairejojo/rhinoimg
+docker run -d -p 80:80 --name jojweb voltairejojo/rhinoimg
+
+
+
+
