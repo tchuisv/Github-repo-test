82e170d458d4de87e0e94336aa9465bf580208f5
tchuiv2 <foffevoltaire@gmail.com>
2024-08-19 21:38:36 -0500

    autodock

--- dockercon.sh
+++ dockercon.sh
@@ -7,7 +7,14 @@ cd dockdir
 yum install docker -y
 systemctl start docker.service
 docker pull voltairejojo/rhinoimg
-docker run -d -p 80:80 --name jojweb voltairejojo/rhinoimg
+echo "what will be your container name"
+read contname
+docker run -d -p 80:80 --name $contname voltairejojo/rhinoimg
+sleep 2
+echo "enter container name to remote into the container"
+sleep 2
+read remotecont
+docker exec -it $remotecont bash
 
 
 
